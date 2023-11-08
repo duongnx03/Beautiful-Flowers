4b3f4f324f21861fdb32afdbb021cd5f2b24e3cd
DuongNX <duongnx170803@gmail.com>
2023-11-08 19:47:49 +0700

    update data product

--- beautiful_flowers/routes/productRouter.js
+++ beautiful_flowers/routes/productRouter.js
@@ -16,5 +16,14 @@ productRouter.get('/admin/edit/:id', getFromEdit);
 productRouter.post('/admin/edit/:id', upload.single('image'), editProduct);
 productRouter.get('/admin/delete/:id', deleteProduct);
 
+productRouter.get('/products', async (req, res) => {
+    try {
+      const products = await getAllProducts();
+      res.render('product', { products }); // Chuyển danh sách sản phẩm vào template
+    } catch (error) {
+      console.error('Error handling products:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 
 module.exports = productRouter;
